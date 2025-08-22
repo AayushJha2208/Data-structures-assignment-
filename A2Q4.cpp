f210ee0e435be0eadac8dc9778f0bd2ff90b5eed
AayushJha2208 <aayushjha2208@gmail.com>
2025-08-22 12:29:03 +0530

    Add files via upload

---
+++ A2Q4.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// int main(){
+//     char dest[50] = "This is an";
+//     char src[50] = " example";
+
+//     strcat(dest, src);
+//     cout << dest ;
+//     return 0;
+// }
+
+// int main(){
+//     string str, result = "";
+//     cout << "Enter a string: ";
+//     getline(cin, str);
+
+//     for (char c : str) {
+//         if (!(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+//               c=='A'||c=='E'||c=='I'||c=='O'||c=='U')) result += c;
+//     }
+
+//     cout << "String without vowels: " << result << endl;
+//     return 0;
+// }
+
+// int main() {
+//     char ch;
+//     cout << "Enter an uppercase character: ";
+//     cin >> ch;
+
+//     if (ch >= 'A' && ch <= 'Z') {
+//         ch = ch + 32;  // ASCII difference
+//     }
+
+//     cout << "Lowercase: " << ch << endl;
+//     return 0;
+// }
+
+// int main(){
+//     string str;
+//     cout << "Enter a string: ";
+//     getline(cin, str);
+//     int n = str.length();
+//     for(int i=0;i<n;i++){
+//        for(int j=i+1;j<n;j++){
+//             if (str[j] < str[i]) {
+//                 char temp = str[i];
+//                 str[i] = str[j];
+//                 str[j] = temp;
+//             }
+//        } 
+//     }
+//     cout << "String sorted in alphabetical order: " << str << endl;
+//     return 0;
+// }
+
